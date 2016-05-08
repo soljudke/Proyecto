eede1823edff333c3f4afeb572613766cceacbf1
soljudke <soljudke@outlook.com>
2016-05-08 12:06:54 -0300

    Casa1
    Que no se haya roto

---
+++ Armando/Armando2/Armando2/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Armando2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnHola = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnHola
+            // 
+            this.btnHola.Location = new System.Drawing.Point(383, 430);
+            this.btnHola.Name = "btnHola";
+            this.btnHola.Size = new System.Drawing.Size(75, 23);
+            this.btnHola.TabIndex = 0;
+            this.btnHola.Text = "button1";
+            this.btnHola.UseVisualStyleBackColor = true;
+            this.btnHola.Click += new System.EventHandler(this.btnHola_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(737, 465);
+            this.Controls.Add(this.btnHola);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHola;
+    }
+}
+
---
+++ Armando/Armando2/Armando2/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+namespace Armando2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHola_Click(object sender, EventArgs e)
+        {
+            palabras pala = new palabras();
+            pala.Traelo();
+            MessageBox.Show(pala.palabra);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GraphicsPath myPath = new GraphicsPath();
+            Rectangle pathRect = new Rectangle(20, 20, 100, 200);
+            myPath.AddRectangle(pathRect);
+
+            
+            Pen myPen = new Pen(Color.Black, 2);
+            e.Graphics.DrawPath(myPen, myPath);
+        }
+    }
+}
