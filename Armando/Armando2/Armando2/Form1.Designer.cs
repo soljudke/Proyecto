4f3dd089375f52b561407b3f3d89b2ae385733d6
soljudke <soljudke@outlook.com>
2016-05-08 14:46:02 -0300

    Fin 8/05

--- Armando/Armando2/Armando2/Form1.Designer.cs
+++ Armando/Armando2/Armando2/Form1.Designer.cs
@@ -30,11 +30,15 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.btnHola = new System.Windows.Forms.Button();
+            this.picPala = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picPala)).BeginInit();
             this.SuspendLayout();
             // 
             // btnHola
             // 
-            this.btnHola.Location = new System.Drawing.Point(383, 430);
+            this.btnHola.Location = new System.Drawing.Point(491, 249);
             this.btnHola.Name = "btnHola";
             this.btnHola.Size = new System.Drawing.Size(75, 23);
             this.btnHola.TabIndex = 0;
@@ -42,23 +46,55 @@
             this.btnHola.UseVisualStyleBackColor = true;
             this.btnHola.Click += new System.EventHandler(this.btnHola_Click);
             // 
+            // picPala
+            // 
+            this.picPala.BackColor = System.Drawing.Color.Transparent;
+            this.picPala.Location = new System.Drawing.Point(491, 84);
+            this.picPala.Margin = new System.Windows.Forms.Padding(5);
+            this.picPala.Name = "picPala";
+            this.picPala.Size = new System.Drawing.Size(164, 143);
+            this.picPala.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picPala.TabIndex = 1;
+            this.picPala.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(214, 305);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(299, 108);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(737, 465);
+            this.ClientSize = new System.Drawing.Size(779, 474);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picPala);
             this.Controls.Add(this.btnHola);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picPala)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnHola;
+        private System.Windows.Forms.PictureBox picPala;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Armando/Armando2/Armando2/Form1.cs
+++ Armando/Armando2/Armando2/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Drawing;
+using System.IO;
 namespace Armando2
 {
     public partial class Form1 : Form
@@ -26,13 +26,33 @@ namespace Armando2
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            GraphicsPath myPath = new GraphicsPath();
-            Rectangle pathRect = new Rectangle(20, 20, 100, 200);
-            myPath.AddRectangle(pathRect);
-
+            palabras pala = new palabras();
+            pala.Traelo();
+            this.picPala.Image = System.Drawing.Image.FromFile("C:/Users/c/Desktop/Proyecto/IMGS/IMGS ARMANDO/"+pala.foto);
+            Label[] labels = new Label[pala.cantLetras];
+            int y=100;
+            int x = 100;
             
-            Pen myPen = new Pen(Color.Black, 2);
-            e.Graphics.DrawPath(myPen, myPath);
+            for (int i = 0; i < pala.cantLetras; i++)
+            {
+                labels[i] = new Label();
+
+                labels[i].Size = new Size(13, 20);
+
+                labels[i].Text = pala.palabra[i].ToString();
+
+               
+                 labels[i].ForeColor = Color.Black;
+
+                labels[i].BackColor = Color.Transparent;
+
+                labels[i].Location = new Point(y, 200);
+
+                this.Controls.Add(labels[i]);
+
+                y = y + 30;
+            }
+            
         }
     }
 }
