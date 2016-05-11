d880ac5dd43012b67df43c2cd89f46fe104e298d
soljudke <41614549@ort>
2016-05-11 13:52:15 -0300

    asdas
    g

--- Armando/Armando/Armando/MainPage.xaml.cs
+++ Armando/Armando/Armando/MainPage.xaml.cs
@@ -26,5 +26,10 @@ namespace Armando
         {
             this.InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            greetingOutput.Text = "Hello, " + nameInput.Text + "!";
+        }
     }
 }
